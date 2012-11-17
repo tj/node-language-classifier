fc26fb7ba430bfd6e65ddfc1686df046be6385ee
TJ Holowaychuk <tj@vision-media.ca>
2012-11-16 16:44:57 -0800

    add objective-c

--- test/index.js
+++ test/index.js
@@ -2,19 +2,5 @@
 var lang = require('..');
 
 describe('should classify', function(){
-  it('ruby', function(){
-    lang('def foo\nend').should.equal('ruby');
-  });
 
-  it('python', function(){
-    lang('for link in links:').should.equal('python');
-  });
-
-  it('javascript', function(){
-    lang('Foo.prototype.bar = function(){}').should.equal('javascript');
-  });
-
-  it('c', function(){
-    lang('#include <stdio.h>').should.equal('c');
-  });
 });
