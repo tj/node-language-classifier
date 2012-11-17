d68c567b95069e6bcb0ad38eda99e21567400b14
TJ Holowaychuk <tj@vision-media.ca>
2012-11-16 17:05:00 -0800

    add new test runner

--- test/index.js
+++ test/index.js
@@ -1,6 +1,19 @@
 
-var lang = require('..');
+var lang = require('..')
+  , fs = require('fs')
+  , path = require('path')
+  , readdir = fs.readdirSync
+  , read = fs.readFileSync;
 
 describe('should classify', function(){
-
+  readdir('test/cases').forEach(function(file){
+    var ext = path.extname(file);
+    var base = path.basename(file, ext);
+    it(ext, function(){
+      file = path.join('test/cases', file);
+      var str = read(file, 'utf8');
+      var ret = lang(str);
+      ret.should.equal(base);
+    })
+  });
 });
